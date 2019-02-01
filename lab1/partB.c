fa73ca93046b16b1d43f0b3f8a1c5f9b7a7e2403
Spencer Goulette <32938977+SpencerGoulette@users.noreply.github.com>
2019-01-31 20:37:33 -0500

    Added colored prints

--- lab1/partB.c
+++ lab1/partB.c
@@ -5,13 +5,23 @@
 #include<stdio.h>
 
 int main(){
+	// int used to change color of prints
+	int color = 30;
 	
 	//Two for loops to print Hello World based on the size of data types
 	printf("Prints Hello World equal to the number of bytes in an int\n");
-	for(int i = 0; sizeof(int) > i; i++)	printf("Hello World\n");
+	for(int i = 0; sizeof(int) > i; i++)
+	{
+		color = 30 + i;
+		printf("\x1b[%dm""Hello World""\x1b[0m""\n", color);
+	}
 
-	printf("Prints Hello World equal to the number of bytes in a long\n");
-	for(int i = 0; sizeof(long) > i; i++)	printf("Hello World\n");
+	printf("Prints Hello World equal to the number of bytes in a long\n")	
+	for(int i = 0; sizeof(long) > i; i++)
+	{
+		color = 30 + i;
+		printf("\x1b[%dm""Hello World""\x1b[0m""\n", color);
+	}
 	
 	return 0;
 }
