919d216bc006e7c265b464b5dac7816fe38887c1
Steven L Ferrarese <steven.ferrarese@maine.edu>
2019-02-10 15:14:10 -0500

    added comments and fixed code

--- lab2/partA.c
+++ lab2/partA.c
@@ -5,180 +5,53 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdint.h>
-void setuppins(char *pins[]);
-void append(char * x, char * xx);
-void unexportpins(char * pins[]);
-void setDirection(char * pins[]);
-int export(char *);
-int unexport(char *);
-int direction(char *, char*);
-int value(char *, char *);
+#include <wiringPi.h>
 int main(int argc, char ** argv)
 {
-	if(argc > 2)
+	//error checking 
+	if(argc > 2)//check amount of arguments
 	{
 		printf("Too many arguments\n");
-		return 3;
+		return 3;//exit program
 	}
-	else if (argc < 2)
+	else if (argc < 2)//not enought arguments
 	{
 		printf("Not enought arguments\n");
-		return 4;
+		return 4;//exit program
 	}	
-	int result = 0;	
-	char * pins[] = {"8","9","7","0","2","3","12","13"};
-	if (((argv[1][0]== '0')&&(argv[1][1]=='x')))
+	
+	int result,x;
+	//setup wiringPi
+	wiringPiSetup();
+	for (int i = 0; i < 8; i++)
+		pinMode(i,OUTPUT);
+	
+	//parse input for LED's
+	if (((argv[1][0]== '0')&&(argv[1][1]=='x')))//if input is in hex
 	{
 		result = strtol(argv[1],NULL,16);
 	}
 	else 
 	{
-		result = atoi(argv[1]);
+		result = atoi(argv[1]);//if a number
 	}
+
 	
-	//setup
-	setuppins(pins);
-	usleep(1000000);
-	setDirection(pins);
-	usleep(1000000);
 
-	value(pins[0],"1");
-
-	for(int i = 0; i < 8; i++)
+	//turn on LED's based on value
+	for(int i = 0; i < 8; i++)//once for each LED
 	{
 	
-		if(result &(0x01 << i ))
+		if(result &(0x80 >> i ))//mask and see if bit in 1 or 0
 		{
 			printf("1");
-			value(pins[i],"1");
+			digitalWrite(i,HIGH);//LED on
 		}
 		else
 		{
 			printf("0");
-			value(pins[i],"0");
+			digitalWrite(i,LOW);//LED off
 		}
 	} 
-
-	unexportpins(pins);		
-
-	return 0;
-}
-void setuppins(char *pins[])
-{
-	int x;
-
-	for (x = 0; x < 8; x++)
-	{
-		export(pins[x]);
-	}
-}
-void setDirection(char *pins[])
-{
-	int x;
-	for (x = 0; x < 8 ;x++)
-	{
-		direction(pins[x],"out");
-	}
-}
-void unexportpins(char *pins[])
-{
-	int x;
-	for(x = 0; x < 8; x++)
-	{
-		unexport(pins[x]);
-	}
-
-}
-//sets up gpio files directory 
-int export(char * pin)
-{
-	char cmd [150] = "cd /sys/class/gpio && echo ";
-	
-	append(cmd, pin);
-	append(cmd, " > export");	
-	
-	//makes call on command line
-	system(cmd);
-	return 0;
-}
-//deletes the gpio pin file directory
-int unexport(char * pin)
-{
-	char cmd [150] = "cd /sys/class/gpio && echo ";
-
-	append(cmd, pin);
-	append(cmd, " > unexport");	
-	
-	//run on command line
-	system(cmd);
-	
-	return 0;
-}
-//sets direction of the gpio pin
-int direction(char * pin, char * dir)
-{
-	//path to file creation
-	char loc[35] = "/sys/class/gpio/gpio\0";
-
-	append(loc, pin);
-	append(loc,"/direction");
-	
-	//open file
-	int file = open(loc,O_WRONLY);
-	
-	if (file < 0)//file not found
-	{
-		printf("Error direction file not found\n");
-		return 2;
-	}
-
-	//open file
-	int x = write(file,dir,strlen(dir));
-	//error checking
-	if (x != strlen(dir))
-	{
-		printf("could not write direction\n");	
-	}
-	close(file);
-	return 0;
-}
-//sets value of gpio pin
-int value(char * pin, char * val)
-{
-	
-	char loc[35] = "/sys/class/gpio/gpio\0";
-
-	append(loc,pin);
-	append(loc,"/value");
-	//open file
-	int file = open(loc,O_WRONLY);
-	//error checking
-	if (file < 0)//file not found
-	{
-		printf("Error value file not found\n");
-		return 2;
-	}
-	
-	// write vale to file
-	int x = write(file,val,strlen(val));
-	
-	if (x != strlen(val))
-	{
-		printf("could not write value\n");	
-		return 1;
-	}
-	//cloase the file
-	close(file);
-	return 0;
-}
-void append(char * x, char * xx)
-{
-	int len = strlen(xx);
-	int len2 = strlen(x);
-	int i;
-	for(i = 0; i < len; i++)
-	{
-		x[len2+i] = xx[i];
-	}
-	x[len2+i] = '\0';
+	return 0;//no errors exit program
 }
