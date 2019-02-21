3ed15e1c6baabd1560fba6e620c0d93fb25b54e8
Stven Ferrarese <stevof12@gmail.com>
2019-02-21 15:01:57 -0500

    lab3 partA

---
+++ lab3/led.c
@@ -0,0 +1,71 @@
+// Lab 2
+// Spencer Goulette, Jason Millette, Steven Ferrarese
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <wiringPi.h>
+int main(int argc, char ** argv)
+{
+	//error checking 
+	if(argc > 2)//check amount of arguments
+	{
+		printf("Too many arguments\n");
+		return 3;//exit program
+	}
+	else if (argc < 2)//not enought arguments
+	{
+		printf("Not enought arguments\n");
+		return 4;//exit program
+	}	
+	
+	int result,x;
+	//setup wiringPi
+	wiringPiSetup();
+	for (int i = 0; i < 8; i++)
+		pinMode(i,OUTPUT);
+	
+	//parse input for LED's
+	//checks for leading 0 (hex or octal)
+	if (argv[1][0] == '0')
+	{
+		if(argv[1][1] == 'x')//check for x (hex)
+			sscanf(argv[1],"0x%X\n", &result);
+		else//octal
+			sscanf(argv[1], "0%o\n", &result);
+	}
+	else if (argv[1][0] == '-')//error check to see if negative number
+	{
+		printf("Error no negative numbers\n");
+		return 2;//exit
+	}	
+	else 
+		sscanf(argv[1],"%d\n",&result);
+	
+
+	//check if input is more than 8 bits
+	if (result > 255)
+	{
+		printf("Error input too large\n");
+		return 5;//exit
+	}
+
+	//turn on LED's based on value
+	for(int i = 0; i < 8; i++)//once for each LED
+	{
+	
+		if(result &(0x80 >> i ))//mask and see if bit in 1 or 0
+		{
+			printf("1");
+			digitalWrite(i,HIGH);//LED on
+		}
+		else
+		{
+			printf("0");
+			digitalWrite(i,LOW);//LED off
+		}
+	} 
+	return 0;//no errors exit program
+}
--- lab3/partA.c
+++ lab3/partA.c
@@ -3,3 +3,52 @@
 //Jason Millette, Spencer Goulette, Steven Ferrarese
 
 #include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+int main()
+{
+	FILE *ff;
+	ff = fopen("prov/loadavg", "r");
+
+	if (ff < 0)
+	{
+		printf("Error opening program\n");
+		return 4;
+	}
+
+	float input = 0;
+	char loadavg[6];
+	while(1)
+	{
+		fopen(loadavg,5,ff);
+		
+		input = atof(loadavg);	
+		
+		printf("%s\n%lf\n", loadavg,input);
+		if (input < 0.125)
+			system("./led 0x01");
+		else if (input < 0.25)
+			system("./led 0x03");
+		else if (input < 0.5)
+			system("./led 0x07");
+		else if (input < 1)
+			system("./led 0x0F");
+		else if (input < 2)
+			system("./led 0x1F");
+		else if (input < 4)
+			system("./led 0x3F");
+		else if (input < 8)
+			system("./led 0x7F");
+		else 
+			sytem("./led 0xFF");
+		
+		sleep(10);
+	}
+	fclose(ff);
+	return 0;
+}
+
+
+
+
