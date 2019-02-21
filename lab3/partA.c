bd88b8e8d35b8d6cc9a2fbdd4f8d56c1d2a260e7
JasonMillette <jason.millette@maine.edu>
2019-02-21 20:28:02 +0000

    fixed repo

---
+++ lab3/partA.c
@@ -0,0 +1,65 @@
+//ECE477 Lab3
+//2/21/19
+//Jason Millette, Spencer Goulette, Steven Ferrarese
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+int main()
+{
+	//Opening /proc/loadavg to read the 1 minute load average
+	FILE *ff;
+	ff = fopen("/proc/loadavg", "r");
+	if (ff < 0) //error checking on file open
+	{
+		printf("Error opening program\n");
+		return 4;
+	}
+
+	//float value and string
+	float input = 0;
+	char loadavg[420];
+
+	while(1) //infinite loop to check value every ten seconds and call led function
+	{
+<<<<<<< HEAD
+		fgets(loadavg, 420, ff); //gets the first 5 characters of the file
+		sscanf(loadavg, "%f ", &input);	//Converts string to float
+
+		printf("/proc/loadavg: %s\n1 minute load average: %lf\n", loadavg,input); //Prints variables for debugging
+
+		//Different cases for loadavg
+=======
+		fgets(loadavg,5,ff);
+		
+		input = atof(loadavg);	
+		
+		printf("%s\n%lf\n", loadavg,input);
+>>>>>>> 5cedbc138634aa14c73997e21d9e020bfcc63906
+		if (input < 0.125)
+			system("./led 0x01");
+		else if (input < 0.25)
+			system("./led 0x03");
+		else if (input < 0.5)
+			system("./led 0x07");
+		else if (input < 1)
+			system("./led 0x0F");
+		else if (input < 2)
+			system("./led 0x1F");
+		else if (input < 4)
+			system("./led 0x3F");
+		else if (input < 8)
+			system("./led 0x7F");
+		else 
+			system("./led 0xFF");
+		
+		sleep(10);
+	}
+	fclose(ff);
+	return 0;
+}
+
+
+
+
